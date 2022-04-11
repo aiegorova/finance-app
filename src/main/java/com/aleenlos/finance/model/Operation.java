fa061f5e7c48b059d17d983cde5ff5a8747bf043
Alena Egorova <alena_egorova@epam.com>
2022-04-11 17:07:12 +0300

    Add getters, setters and constructors to entities

--- src/main/java/com/aleenlos/finance/model/Operation.java
+++ src/main/java/com/aleenlos/finance/model/Operation.java
@@ -1,15 +1,21 @@
 package com.aleenlos.finance.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 
 @Entity
+@Data
 public class Operation {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @ManyToOne
