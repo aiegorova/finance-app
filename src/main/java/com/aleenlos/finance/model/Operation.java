90fcadef68fe8a34144014a83df90c3d467037e4
Alena Egorova <alena_egorova@epam.com>
2022-04-11 21:31:47 +0300

    Add constructors and builders to entities

--- src/main/java/com/aleenlos/finance/model/Operation.java
+++ src/main/java/com/aleenlos/finance/model/Operation.java
@@ -1,6 +1,9 @@
 package com.aleenlos.finance.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +15,9 @@ import java.math.BigDecimal;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Operation {
 
     @Id
