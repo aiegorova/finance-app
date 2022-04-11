c55fe7ba165eeaf57add5725b6c77c7de31c3d7a
Alena Egorova <ermilovaalena@gmail.com>
2022-04-11 20:48:57 +0300

    Fin-003: Add DAO layer (#2)
    * Add DAO layer
    
    * Add finding by phone number for account
    
    Co-authored-by: Alena Egorova <alena_egorova@epam.com>

---
+++ src/main/java/com/aleenlos/finance/dao/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.aleenlos.finance.dao;
+
+import com.aleenlos.finance.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
