63fcd82359b37a6c36a21bd4783429b58a2b4cf7
Alena Egorova <alena_egorova@epam.com>
2022-04-11 20:47:27 +0300

    Add class for test constants

---
+++ src/test/java/com/aleenlos/finance/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.aleenlos.finance.utils;
+
+public class Constants {
+
+    public static final Long CORRECT_ID = 1L;
+    public static final String CORRECT_PHONE_NUMBER = "9111111111";
+
+}
