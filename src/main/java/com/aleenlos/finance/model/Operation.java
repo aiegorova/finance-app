d98a6f5d79f7795af05f81b76ba2b53ebc4fa51e
Alena Egorova <ermilovaalena@gmail.com>
2022-04-11 22:46:08 +0300

    Fix id generation strategy (#6)
    Co-authored-by: Alena Egorova <alena_egorova@epam.com>

--- src/main/java/com/aleenlos/finance/model/Operation.java
+++ src/main/java/com/aleenlos/finance/model/Operation.java
@@ -21,7 +21,7 @@ import java.math.BigDecimal;
 public class Operation {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
