03be9428166c0fbde0c368480ddba3cec672d641
Alena Egorova <alena_egorova@epam.com>
2022-04-11 22:23:59 +0300

    Change entities to DTO in service layer

--- src/main/java/com/aleenlos/finance/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/aleenlos/finance/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.aleenlos.finance.service.impl;
 
 import com.aleenlos.finance.dao.CategoryRepository;
+import com.aleenlos.finance.dto.CategoryDTO;
+import com.aleenlos.finance.mapper.CategoryMapper;
 import com.aleenlos.finance.model.Category;
 import com.aleenlos.finance.service.CategoryService;
 import lombok.RequiredArgsConstructor;
@@ -8,26 +10,34 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
 
     @Override
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream()
+                .map(categoryMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Optional<Category> findById(Long id) {
-        return categoryRepository.findById(id);
+    public Optional<CategoryDTO> findById(Long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return category.map(categoryMapper::toDTO);
     }
 
     @Override
-    public Category save(Category category) {
-        return categoryRepository.saveAndFlush(category);
+    public CategoryDTO save(CategoryDTO dto) {
+        Category category = categoryMapper.toEntity(dto);
+        categoryRepository.saveAndFlush(category);
+        return categoryMapper.toDTO(category);
     }
 
 }
